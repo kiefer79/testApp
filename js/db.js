8d92ceba81d545c3353fb10acf25d5dd098edd67
Andreas Kiefer <kiefer@rootality.de>
2013-02-08 09:53:16 +0100

    project start

---
+++ js/db.js
@@ -0,0 +1,59 @@
+function insertNews(tx, uid, data) {
+	//tx.executeSql('INSERT INTO news (uid, data) VALUES (1, "Test1")');
+	//tx.executeSql('INSERT INTO news (uid, data) VALUES (2, "Test2")');
+	//tx.executeSql('INSERT INTO news (uid, data) VALUES (3, "Test3")');
+	tx.executeSql('INSERT INTO news (uid, data) VALUES (3, "Test3")');
+	
+	
+}
+
+function dbError(err) {
+	alert("Error processing SQL: " + err.code);
+}
+
+function dbSuccess() {
+	alert('db success');
+}
+
+function createTables(tx) {
+	console.log('createTables');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS news (uid unique, data)');
+}
+
+function queryNews(tx) {
+	console.log('queryNews');
+	tx.executeSql('SELECT * from news', [], newsQuerySuccess, dbError);
+}
+
+function newsQuerySuccess(tx, results) {
+	console.log('newsQuerySuccess');
+	// process only if valid results
+	if(typeof results != 'undefined'){
+		len = results.rows.length;
+		console.log(len);
+		console.log(results.rows.item(0).data.uid);
+
+		for (var i=0; i<len; i++){
+				console.log(results.rows.item(i).data);
+				newitem = $('#newstemplate').clone();
+				newitem.find('.date').text(row.datetime);
+				newitem.find('.title').text(row.title);
+				newitem.find('.teaser').text(row.bodytext);
+				jQuery('#newslist').append(newitem.html());
+				
+		}
+	}
+}
+
+function newsQueryError() {
+	alert('news query error');
+}
+
+function getLastID(tx) {
+	tx.executeSql('SELECT max(uid) from news', [], lastIDSuccess, dbError);
+}
+
+function lastIDSuccess(tx, results) {
+	console.log('lastIDSuccess');
+	console.log(results.rows.item(0).data);
+}
